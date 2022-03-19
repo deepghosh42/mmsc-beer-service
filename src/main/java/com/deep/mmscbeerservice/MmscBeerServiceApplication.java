871b115de487b3bd69fe241e8466b2d24ce97dab
deepg <deepghosh42@gmail.com>
2022-03-20 02:21:13 +0530

    Initail Code base.closes #1

---
+++ src/main/java/com/deep/mmscbeerservice/MmscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deep.mmscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmscBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deep/mmscbeerservice/MmscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.mmscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
